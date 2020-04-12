f93be41f43358bf5ea1a986f741f9758a1ffa462
mahim <ashrafulmahim@gmail.com>
2020-04-12 22:40:15 +0600

    project object created with validation

---
+++ ppmtool/src/main/java/com/mahim/ppmtool/services/ProjectService.java
@@ -0,0 +1,17 @@
+package com.mahim.ppmtool.services;
+
+import com.mahim.ppmtool.domain.Project;
+import com.mahim.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project) {
+        return projectRepository.save(project);
+    }
+}

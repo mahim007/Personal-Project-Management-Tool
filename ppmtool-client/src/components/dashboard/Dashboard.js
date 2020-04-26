5eab0009926fa01ff4d6a1697e40d0ce89fdeba9
mahim <ashrafulmahim@gmail.com>
2020-04-26 20:50:35 +0600

    project CRUD operationd are finised!
    all operations related with project such as cread, read, update and delete are done, one little bud in updating project is still there. will come back to it later.

---
+++ ppmtool-client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import ProjectItem from "../project/ProjectItem";
+import CreateProjectButton from "../project/CreateProjectButton";
+import { getProjects } from "../../actions/ProjectActions";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+class Dashboard extends Component {
+  componentDidMount() {
+    this.props.getProjects();
+  }
+
+  render() {
+    const { projects } = this.props.project;
+    return (
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <CreateProjectButton />
+              <br />
+              <hr />
+              {projects.map((project) => (
+                <ProjectItem
+                  key={project.projectIdentifier}
+                  project={project}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Dashboard.propTypes = {
+  project: PropTypes.object.isRequired,
+  getProjects: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  project: state.project,
+});
+
+export default connect(mapStateToProps, { getProjects })(Dashboard);
